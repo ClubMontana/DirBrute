49d85e4ba0f478bc8f9058fb1d706bcbd49b2c42
xer0dayz <1N3@hushmail.com>
2021-03-05 15:29:30 -0700

    Update main.go

--- main.go
+++ main.go
@@ -34,17 +34,6 @@ var Dark = "\033[90m"
 var clear map[string]func() //create a map for storing clear funcs
 
 func init() {
-	clear = make(map[string]func()) //Initialize it
-	clear["linux"] = func() {
-		cmd := exec.Command("clear") //Linux example, its tested
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-	clear["windows"] = func() {
-		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
 	if runtime.GOOS == "windows" {
 		Reset = ""
 		Dark = ""
